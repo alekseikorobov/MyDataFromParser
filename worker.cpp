46543e36279d831273be4661e22b2419431f7240
acer76 <cezar0@list.ru>
2016-08-29 17:44:23 +0300

    Исправлен вывод количества полей характеристик
    Теперь добавление в словарь уникальных значений характеристик происходит по бинарному полю, чтобы distinct не сцеплял регистр или специальные символы

--- worker.cpp
+++ worker.cpp
@@ -149,7 +149,7 @@ QString worker::myReplace(QString str){
 
 int worker::findHar(QString str){
     for (int i = 0; i < har.size(); ++i)
-        if(har[i].key == str.toLower()) return i;
+        if(har[i].key == str) return i;
     return -1;
 }
 QString worker::getStringJoinVal(){
@@ -188,30 +188,19 @@ QString worker::getKey(QString str){
         har[key]=";";
     }*/
 
-    //далее заполняем все значения, если есть - обновляем, если нет - добавляем в конец
+    //далее заполняем все значения, если есть - обновляем, если нет - говорим об ошибки
     while ((pos = rx.indexIn(str, pos)) != -1) {
 
         QString key = rx.cap(1);
         QString val = rx.cap(2);
-        //qDebug() << key << " " <<val;
-        data dt = data(key.toLower(),val + ";");
-        int index = findHar(key);
-        if(index==-1) qDebug() << "  Поле не найдено! " <<key;
-        //else har.append(dt);
+        int index = findHar(key.toLower().trimmed());
+        if(index==-1) listError.append( "Поле не найдено!-" + key.toLower().trimmed());
+        else har[index].val = val+ ";";
 
         pos += rx.matchedLength();
     }
-    ///при возвращении добавить число действительных полей
-    /// (то есть количество ; после последнего поля)
-    /// с помощью запроса - select count(distinct name) from temp_data;
 
-    //countPoles = countPoles-(har.size()-1); //количество характеристик должно быть на 1 меньше
-    //QString emptPole = "";
-
-    /*for (int x = 0; x < countPoles; ++x) {
-        emptPole += ";";
-    }*/
-    //вернуть форматированные значения и плюс пустые поля
+    //вернуть форматированные значения
     return getStringJoinVal();
 }
 
@@ -233,30 +222,22 @@ void worker::Scan(){
                  " ALTER TABLE skus_temp ADD UNIQUE INDEX in_id (id ASC); "
                  " ALTER TABLE skus_temp ADD INDEX in_prod (prod_id ASC); "
                  ))
-        {
-            listError.append("Не удалось выполнить запрос create table skus_temp" + db->lastError);
-            emit onComplit();
-            return;
-        }
-        if(!db->exec("truncate table skus_temp;"))
-        {
-            listError.append("Не удалось выполнить запрос truncate table skus_temp;");
-            emit onComplit();
-            return;
-        }
-    //db->exec("alter table product add art nvarchar(100);");
-
+    {
+        listError.append("Не удалось выполнить запрос create table skus_temp" + db->lastError);
+        emit onComplit();
+        return;
+    }
+    if(!db->exec("truncate table skus_temp;"))
+    {
+        listError.append("Не удалось выполнить запрос truncate table skus_temp;");
+        emit onComplit();
+        return;
+    }
     if(!db->query->exec("SET SQL_SAFE_UPDATES = 0;")){
         listError.append("Не удалось выполнить запрос SET SQL_SAFE_UPDATES = 0;");
         emit onComplit();
         return;
     }
-//    if(!db->query->exec("update product set art = concat('1',LPAD(id,5,'0')) where art is null ;")){
-//        listError.append("Не удалось выполнить запрос update proc");
-//        emit onComplit();
-//        return;
-//    }
-
     foreach (string str, sl) {
         QStringList s = str.split(seporator);
         string art="",pr="",c="";
@@ -268,7 +249,7 @@ void worker::Scan(){
         if(s.size() == 4){
             art = s[0];
             pr = string(s[1]) +string(",") +string(s[2]);
-            c = s[3];            
+            c = s[3];
             db->query->exec(string("insert into skus_temp(name,prise,count) values('%1','%2',%3)").arg(art,pr,c));
         }
 
@@ -292,27 +273,27 @@ void worker::Scan(){
 
     //обновление продуктов
     string qup ="update skus_temp s "
-            " join (select p.id,v.har_value"
-            " 		from hars_values v  join hars h on v.har_id = h.id"
+                " join (select p.id,v.har_value"
+                " 		from hars_values v  join hars h on v.har_id = h.id"
                 " 						join product p on v.prod_id=p.id and h.cat_id = p.cat_id "
                 " where h.name = 'Артикул') v1 on v1.har_value = s.name"
-              " set prod_id = v1.id;";
+                " set prod_id = v1.id;";
 
-     if(!db->query->exec(qup)){
-         listError.append("Не удалось выполнить запрос update skus_temp s");
-         emit onComplit();
-         return;
-     }
+    if(!db->query->exec(qup)){
+        listError.append("Не удалось выполнить запрос update skus_temp s");
+        emit onComplit();
+        return;
+    }
     string temp = "create temporary table if not exists temp_data(id int,har_value nvarchar(500),name nvarchar(500),prod nvarchar(500),image nvarchar(255),brand nvarchar(255)) "
-            " DEFAULT CHARACTER SET cp1251 COLLATE cp1251_general_ci; "
-            " ALTER TABLE temp_data ADD INDEX in_prod_id (id ASC); "
-            " ALTER TABLE temp_data ADD INDEX in_name (`name` ASC); "
-            " truncate table temp_data; "
-            " insert into temp_data(id,har_value,name,prod,image,brand)  "
-            " select p.id,v.har_value,h.name,p.name prod,p.image,p.brand "
+                  " DEFAULT CHARACTER SET cp1251 COLLATE cp1251_general_ci; "
+                  " ALTER TABLE temp_data ADD INDEX in_prod_id (id ASC); "
+                  " ALTER TABLE temp_data ADD INDEX in_name (`name` ASC); "
+                  " truncate table temp_data; "
+                  " insert into temp_data(id,har_value,name,prod,image,brand)  "
+                  " select p.id,v.har_value,h.name,p.name prod,p.image,p.brand "
                   "from hars_values v join hars h on v.har_id = h.id    "
-                                  " join product p on v.prod_id=p.id and h.cat_id = p.cat_id "
-            " where p.id in (select prod_id from skus_temp);";
+                  " join product p on v.prod_id=p.id and h.cat_id = p.cat_id "
+                  " where p.id in (select prod_id from skus_temp);";
 
     if(!db->query->exec(temp)){
         listError.append("Не удалось создать временную таблицу temp_data");
@@ -320,163 +301,137 @@ void worker::Scan(){
         return;
     }
 
-    string getcountString = "select distinct name from temp_data;";
+    string getcountString = "select distinct(BINARY trim(lower(name))),trim(lower(name)) from temp_data;";
     if(!db->exec(getcountString)){
         listError.append("Не удалось выполнить запрос select distinct name from temp_data; - " + db->lastError);
-         emit onComplit();
+        emit onComplit();
         return;
     }
-    //int countHaractKey = 0;
     while(db->query->next()){
         har.append(data(myReplace(db->query->value(0).toString().toLower() ),";")); ///минус 2 потому что разделителей на 1
-                                                        ///меньше и одна характеристика это всегда артикул*/
+        ///меньше и одна характеристика это всегда артикул*/
 
     }
-    //qDebug() << "countHaractKey" << " " << countHaractKey;
-
-     if(!db->query->exec("SET global group_concat_max_len = 18446744073709551615;")){
-              listError.append("Не удалось выполнить SET global group_concat_max_len =");
-              emit onComplit();
-              return;
-          }
-     //добавить проверку на дубли. и если есть дубли, добавить внутренний артикул
-     /*string q = "select t1.id,t1.prod,t1.artic,concat('1',LPAD(t1.id,5,'0')) art,t1.prise,t1.count,t1.htm,t1.brand,"
-             "      concat(t1.image,ifnull(concat(';',GROUP_CONCAT(i.name SEPARATOR ';')),'') ) as i  "
-             " from ( "
-             " SELECT dt.id,dt.prod,st.name artic,st.prise,st.count,dt.image,dt.brand,"
-             //" GROUP_CONCAT(concat('<li><b>',replace(dt.name,'&#730;',''),'</b> : ',replace(replace(dt.har_value,'&nbsp;',''),';',',')) SEPARATOR  '</li>')  as xm,  "
-             " GROUP_CONCAT(concat('<tr><td>',dt.name,'</td><td>',dt.har_value,'</td>') SEPARATOR '</tr>')  as htm "
-             " FROM (select id,har_value,name,prod,image,brand from temp_data where name <> 'Артикул' ) as dt  "
-             "                       join skus_temp st on st.prod_id = dt.id "
-             " group by dt.id,dt.prod,st.name,st.prise,st.count,dt.image,dt.brand) t1 left join images i on t1.id = i.prod_id  "
-             " group by t1.id,t1.prod,t1.artic,t1.prise,t1.count,t1.htm,t1.image,t1.brand"
-             " order by t1.id;";
-     */
-     string q = "select t1.id,ifnull(concat(t2.name,' (',concat('1',LPAD(t1.id,5,'0')),')'),t1.prod) prod,t1.artic,concat('1',LPAD(t1.id,5,'0')) art,t1.prise,t1.count,t1.htm,t1.brand, "
-           " concat(t1.image,ifnull(concat(';',GROUP_CONCAT(i.name SEPARATOR ';')),'') ) as i "
-           " from ( "
-           " SELECT dt.id,dt.prod,st.name artic,st.prise,st.count,dt.image,dt.brand "
-           " ,GROUP_CONCAT(concat('<tr><td>',dt.name,'</td><td>',dt.har_value,'</td>') SEPARATOR '</tr>')  as htm  "
-           " FROM (select id,har_value,name,prod,image,brand from temp_data where name <> 'Артикул' ) as dt   "
-           "                    join skus_temp st on st.prod_id = dt.id  "
-           " group by dt.id,dt.prod,st.name,st.prise,st.count,dt.image,dt.brand) t1 left join images i on t1.id = i.prod_id   "
-           "         left join (select name from product group by name having count(*)>1) t2 on t1.prod= t2.name "
-           " group by t1.id,t1.prod,t2.name,t1.artic,t1.prise,t1.count,t1.htm,t1.image,t1.brand "
-           " order by t1.id;";
+    if(!db->query->exec("SET global group_concat_max_len = 18446744073709551615;")){
+        listError.append("Не удалось выполнить SET global group_concat_max_len =");
+        emit onComplit();
+        return;
+    }
+    string q = "select t1.id,ifnull(concat(t2.name,' (',concat('1',LPAD(t1.id,5,'0')),')'),t1.prod) prod,t1.artic,concat('1',LPAD(t1.id,5,'0')) art,t1.prise,t1.count,t1.htm,t1.brand, "
+               " concat(t1.image,ifnull(concat(';',GROUP_CONCAT(i.name SEPARATOR ';')),'') ) as i "
+               " from ( "
+               " SELECT dt.id,dt.prod,st.name artic,st.prise,st.count,dt.image,dt.brand "
+               " ,GROUP_CONCAT(concat('<tr><td>',dt.name,'</td><td>',dt.har_value,'</td>') SEPARATOR '</tr>')  as htm  "
+               " FROM (select id,har_value,name,prod,image,brand from temp_data where name <> 'Артикул' ) as dt   "
+               "                    join skus_temp st on st.prod_id = dt.id  "
+               " group by dt.id,dt.prod,st.name,st.prise,st.count,dt.image,dt.brand) t1 left join images i on t1.id = i.prod_id   "
+               "         left join (select name from product group by name having count(*)>1) t2 on t1.prod= t2.name "
+               " group by t1.id,t1.prod,t2.name,t1.artic,t1.prise,t1.count,t1.htm,t1.image,t1.brand "
+               " order by t1.id;";
 
     if(!db->exec(q)){
         listError.append("Не удалось выполнить запрос выборки всех данных - " + db->lastError);
-         emit onComplit();
+        emit onComplit();
         return;
     }
-    //int i = 0;
-    //getStringJoinKey()
-        /// вставить полученные поля, после прохода всех строк
-        /// КУДА ВСТАВЛЯТЬ БРЕНД ПОКА НЕПОНЯТНО!
-
-            QStringList listRow;
-            QString n = QString(";");
-            while(db->query->next()){
-
-                QString line;
-
-                //t1.id 0,t1.prod 1,t1.artic 2 ,art 3 ,t1.prise 4 ,t1.count 5 ,t1.htm 6 ,t1.brand 7 , i 8
-
-                line = myReplace(db->query->value(1).toString()) + n;///prod "Наименование"
-                line += db->query->value(2).toString() + n;/// art "Артикула"
-                QString qst = db->query->value(3).toString();
-                line +=  qst + n;///artic "Наименование артикул"
-                line +=  "RUB;";                                                        ///"Валюта"
-                line +=  db->query->value(4).toString() + n;/// prise "Цена"
-                line +=  "1;";///"Доступен для заказа")<<
-                line += "0;";///"Зачеркнутая цена")<<
-                line += "0;";///"Закупочная цена"
-                line +=  db->query->value(5).toString() + n;/// count "В наличии"
-                line += n;//Краткое описание;
-                line += n;//Описание;
-                //QString("Бренд") << db->query->value(7).toString()
-                line +=  n;/// "Наклейка")<<
-                line +=  QString("1;");/// "Статус")<<
-                line +=  n;/// "Тип товаров")<<
-                line +=  n;/// "Теги")<<
-                line += n;/// "Облагается налогом")<<
-                line +=  db->query->value(1).toString() + n;///prod "Заголовок")<<
-                line +=  db->query->value(1).toString() + n;///prod "META Keywords")<<
-                line +=  db->query->value(1).toString() + n;///prod "META Description")<<
-                line +=  n;/// "Ссылка на витрину")<<
-                line +=  n;/// "Дополнительные параметры" << n;
-                //line += n; //db->query->value(8).toString()<< n;/// "Бренд")<< //специально оставляем пустым, чтобы на сайте отображалось корректно
-                line += getKey(myReplace(db->query->value(6).toString() + QString("</tr>")));/// htm "Описание"
-                line += db->query->value(7).toString() + n;/// "Бренд"
-                line += db->query->value(8).toString();/// img(;)
-                line += QString("\n");
-
-                listRow.append(line);
-            }
-
-            QTextStream out(&file);
-            out.setCodec("windows-1251");
-            out  <<QString("Наименование;")<<
-                   QString("Наименование артикула;")<<
-                   QString("Артикул;")<<
-                   QString("Валюта;")<<
-                   QString("Цена;")<<
-                   QString("Доступен для заказа;")<<
-                   QString("Зачеркнутая цена;")<<
-                   QString("Закупочная цена;")<<
-                   QString("В наличии;")<<
-                   QString("Краткое описание;")<<
-                   QString("Описание;")<<
-                   QString("Наклейка;")<<
-                   QString("Статус;")<<
-                   QString("Тип товаров;")<<
-                   QString("Теги;")<<
-                   QString("Облагается налогом;")<<
-                   QString("Заголовок;")<<
-                   QString("META Keywords;")<<
-                   QString("META Description;")<<
-                   QString("Ссылка на витрину;")<<
-                   QString("Дополнительные параметры;")
-                   << getStringJoinKey() ///все поля из характеристик
-                   <<QString("Бренд");
-
-                    for (int k = 0; k < 65; ++k) {
-                        out << QString(";Изображения");
-                    }
-                    out << QString("\n");
-            foreach (QString line1, listRow) {
-                out  << line1;
-            }
 
 
-            file.close();
+    QStringList listRow;
+    QString n = QString(";");
+    while(db->query->next()){
 
+        QString line;
 
-            QString q1 = "select name,prise,count from skus_temp where prod_id is null;";
+        //t1.id 0,t1.prod 1,t1.artic 2 ,art 3 ,t1.prise 4 ,t1.count 5 ,t1.htm 6 ,t1.brand 7 , i 8
 
-            if(!db->exec(q1)){
-                listError.append("Не удалось выполнить запрос выборки всех данных - " + db->lastError);
-                 emit onComplit();
-                return;
-            }
-            while(db->query->next()){
-                listNot.append(db->query->value(0).toString() +string(seporator)+
-                                    db->query->value(1).toString() +string(seporator)+
-                                    db->query->value(2).toString()
-                                       );
-            }
+        line = myReplace(db->query->value(1).toString()) + n;///prod "Наименование"
+        line += db->query->value(2).toString() + n;/// art "Артикула"
+        QString qst = db->query->value(3).toString();
+        line +=  qst + n;///artic "Наименование артикул"
+        line +=  "RUB;";                                                        ///"Валюта"
+        line +=  db->query->value(4).toString() + n;/// prise "Цена"
+        line +=  "1;";///"Доступен для заказа")<<
+        line += "0;";///"Зачеркнутая цена")<<
+        line += "0;";///"Закупочная цена"
+        line +=  db->query->value(5).toString() + n;/// count "В наличии"
+        line += n;//Краткое описание;
+        line += n;//Описание;
+        line +=  n;/// "Наклейка")<<
+        line +=  QString("1;");/// "Статус")<<
+        line +=  n;/// "Тип товаров")<<
+        line +=  n;/// "Теги")<<
+        line += n;/// "Облагается налогом")<<
+        line +=  db->query->value(1).toString() + n;///prod "Заголовок")<<
+        line +=  db->query->value(1).toString() + n;///prod "META Keywords")<<
+        line +=  db->query->value(1).toString() + n;///prod "META Description")<<
+        line +=  n;/// "Ссылка на витрину")<<
+        line +=  n;/// "Дополнительные параметры" << n;
+        line += getKey(myReplace(db->query->value(6).toString() + QString("</tr>")));/// htm "Описание"
+        line += db->query->value(7).toString() + n;/// "Бренд"
+        line += db->query->value(8).toString();/// img(;)
+        line += QString("\n");
 
-            QString q2 = "select id,min(har_value) from temp_data group by id having count(*) = 1 and min(`name`) = 'Артикул'";
+        listRow.append(line);
+    }
 
-            if(!db->exec(q2)){
-                listError.append("Не удалось выполнить запрос выборки данных temp_data - " + db->lastError);
-                 emit onComplit();
-                return;
-            }
-            while(db->query->next()){
-                listNotHaract.append(db->query->value(1).toString());
-            }
-            emit onComplit();
+    QTextStream out(&file);
+    out.setCodec("windows-1251");
+    out  <<QString("Наименование;")<<
+           QString("Наименование артикула;")<<
+           QString("Артикул;")<<
+           QString("Валюта;")<<
+           QString("Цена;")<<
+           QString("Доступен для заказа;")<<
+           QString("Зачеркнутая цена;")<<
+           QString("Закупочная цена;")<<
+           QString("В наличии;")<<
+           QString("Краткое описание;")<<
+           QString("Описание;")<<
+           QString("Наклейка;")<<
+           QString("Статус;")<<
+           QString("Тип товаров;")<<
+           QString("Теги;")<<
+           QString("Облагается налогом;")<<
+           QString("Заголовок;")<<
+           QString("META Keywords;")<<
+           QString("META Description;")<<
+           QString("Ссылка на витрину;")<<
+           QString("Дополнительные параметры;")
+        << getStringJoinKey() ///все поля из характеристик
+        <<QString("Бренд");
+
+    for (int k = 0; k < 65; ++k) {
+        out << QString(";Изображения");
+    }
+    out << QString("\n");
+    foreach (QString line1, listRow) {
+        out  << line1;
+    }
+    file.close();
+    QString q1 = "select name,prise,count from skus_temp where prod_id is null;";
+    if(!db->exec(q1)){
+        listError.append("Не удалось выполнить запрос выборки всех данных - " + db->lastError);
+        emit onComplit();
+        return;
+    }
+    while(db->query->next()){
+        listNot.append(db->query->value(0).toString() +string(seporator)+
+                       db->query->value(1).toString() +string(seporator)+
+                       db->query->value(2).toString()
+                       );
+    }
+
+    QString q2 = "select id,min(har_value) from temp_data group by id having count(*) = 1 and min(`name`) = 'Артикул'";
+
+    if(!db->exec(q2)){
+        listError.append("Не удалось выполнить запрос выборки данных temp_data - " + db->lastError);
+        emit onComplit();
+        return;
+    }
+    while(db->query->next()){
+        listNotHaract.append(db->query->value(1).toString());
+    }
+    emit onComplit();
 }
 
 bool worker::DeleteEmptyCat(){
